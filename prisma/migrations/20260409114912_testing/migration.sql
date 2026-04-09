dbb759ea162dc73d68d508b255cc790a49456701
Praisecodes <cmancoder@gmail.com>
2026-04-09 12:54:36 +0100

    op: setup prisma differently

--- prisma/migrations/20260409114912_testing/migration.sql
+++ prisma/migrations/20260409114912_testing/migration.sql
@@ -1,12 +1,18 @@
 -- CreateEnum
 CREATE TYPE "Platform" AS ENUM ('x', 'instagram', 'facebook', 'tiktok');
 
+-- CreateEnum
+CREATE TYPE "OTPType" AS ENUM ('verify', 'reset');
+
 -- CreateTable
 CREATE TABLE "Users" (
     "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
     "firstName" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
     "username" TEXT NOT NULL,
+    "emailVerified" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -39,11 +45,30 @@ CREATE TABLE "Platforms" (
     CONSTRAINT "Platforms_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Otps" (
+    "id" TEXT NOT NULL,
+    "otp" TEXT NOT NULL,
+    "type" "OTPType" NOT NULL,
+    "validTill" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Otps_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Platforms_clientId_platform_key" ON "Platforms"("clientId", "platform");
 
 -- AddForeignKey
-ALTER TABLE "Clients" ADD CONSTRAINT "Clients_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Clients" ADD CONSTRAINT "Clients_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Platforms" ADD CONSTRAINT "Platforms_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "Clients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Platforms" ADD CONSTRAINT "Platforms_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "Clients"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Otps" ADD CONSTRAINT "Otps_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
